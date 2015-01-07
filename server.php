60b020564824d5467097357a20f22c391065cdf3
Arjun Yonjan <a1y25ory2a@gmail.com>
2015-01-07 18:25:32 +0545

    updated with new improved code....

--- server.php
+++ server.php
@@ -3,6 +3,6 @@
 	$tmp_file = $_FILES['image']['tmp_name'];
 	$filename = $_FILES['image']['name'];
 
-	move_uploaded_file($tmp_file, 'upload_folder/'.$filename);
+	move_uploaded_file($tmp_file, 'upload_folder/'. $filename);
 
 ?>
